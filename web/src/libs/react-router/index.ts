3cb60f21e6985567b8dfe2cee7c2667707b557ee
umaidashi <umaidashi18@gmail.com>
2024-11-03 10:37:09 +0900

    sample fetch

---
+++ web/src/libs/react-router/index.ts
@@ -0,0 +1,11 @@
+import { QueryClient } from '@tanstack/react-query'
+
+export const QUERY_CLIENT = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+    },
+  },
+})
+
